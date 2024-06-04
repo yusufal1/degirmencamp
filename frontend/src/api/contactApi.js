ca69bb841e4e1fc20389ab8fecd84f62d6a8583f
Yusuf AL <yusufal5558@gmail.com>
2024-06-04 18:38:11 +0300

    backend api added

--- frontend/src/api/contactApi.js
+++ frontend/src/api/contactApi.js
@@ -1,6 +1,5 @@
-// frontend/src/api/contactApi.js
 import axios from 'axios';
 
 export const sendContactForm = async (formData) => {
-  return await axios.post('http://localhost:5000/api/contact', formData); // Backend URL'iniz
+  return await axios.post('https://degirmencamp-backend.onrender.com/', formData);
 };
