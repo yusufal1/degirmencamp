b6ae1d203711841c260d4f9b26253e9bdf656f41
Yusuf AL <yusufal5558@gmail.com>
2024-06-05 16:24:18 +0300

    prepare for deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from  './redux/store';
 import './i18n';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
