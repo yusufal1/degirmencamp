dff35062a66397817afc4a0bf971cfb3de3614a6
Yusuf AL <yusufal5558@gmail.com>
2024-06-05 18:52:33 +0300

    cors setting

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express()
 
 //Middleware
-app.use(cors({ origin: 'https://degirmencamp.vercel.app/' }));
+app.use(cors({ origin: 'https://degirmencamp.vercel.app' }));
 app.use(express.json());
 app.use(express.static('public'));
 
